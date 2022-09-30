aecbe760058fc02ebf051756ba8a00e4dc704428
OrlandoPiri <piripiri.orlando@gmail.com>
2022-09-30 21:02:59 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+// import Dashboard from './Dashboard';
+import ProtectedRoute from './ProtectedRoute';
+import Error from './Error';
+import Landing from './Landing';
+import Register from './Register';
+
+export { Landing, Error, Register, ProtectedRoute };
